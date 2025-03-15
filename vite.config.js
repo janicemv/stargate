8ae227ba7b1257a2c307800c7b3796c5d2d19115
Janice <janice.vilela@gmail.com>
2025-03-15 16:11:58 +0100

    treating png

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            // Usar assetsInclude para permitir que PNGs sejam tratados corretamente como assets
-            assetsInclude: ['**/*.PNG']
-        }
-    }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['resources/images/screenshots/*'], // Ignora todos os arquivos da pasta de screenshots
+    },
+  },
 });
-
